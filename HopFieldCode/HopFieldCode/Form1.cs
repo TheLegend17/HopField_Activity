959510099db199e28b0bbde637207b512217ac57
Roberto T. Abellar Jr <roberto.abellarjr@cit.edu>
2023-02-13 22:19:21 +0800

    Add Comments

--- HopFieldCode/HopFieldCode/Form1.cs
+++ HopFieldCode/HopFieldCode/Form1.cs
@@ -1,16 +1,7 @@
-﻿using MathNet.Numerics.LinearAlgebra.Complex;
-using MathNet.Numerics.Optimization.LineSearch;
-using MathNet.Numerics.Providers.FourierTransform;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HopFieldCode
@@ -49,25 +40,23 @@ namespace HopFieldCode
             weights.Add(wt9);
         }
 
-        private void button10_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Changes the color of a button to either Black or White
+        /// </summary>
+        /// <param name="button"> Input button </param>
+        private void ButtonColorChange(Button button)
         {
-            //Get all the boxes value 1 for black 0 for white
-            var buttonsOnForm = this.Controls.OfType<Button>();
-            var listOfButtons = buttonsOnForm.ToList();
-            listOfButtons.RemoveAt(0);
-            listOfButtons.RemoveAt(0);
-            listOfButtons.Reverse();
-            foreach (var btn in listOfButtons)
-            {
-                if (btn.BackColor == Color.Black)
-                    UIinput.Add(1);
-                else
-                    UIinput.Add(0);
-            }
-            var res = Calculate(UIinput);
-            textBox1.Text = res;
+            if (button.BackColor == Color.White)
+                button.BackColor = Color.Black;
+            else
+                button.BackColor = Color.White;
         }
 
+        /// <summary>
+        /// Calculates the hopfield 
+        /// </summary>
+        /// <param name="input">Given input from the 3x3 buttons</param>
+        /// <returns>Returns the correspending string output</returns>
         private String Calculate(List<int> input)
         {
             int temp;
@@ -75,8 +64,13 @@ namespace HopFieldCode
             var inp = input.ToArray();
             bool isPlus = true;
             bool isMinus = true;
+            int ctr = 0;
+
             while (true)
             {
+                ctr++;
+
+                // Actual Calculation
                 foreach (var weight in weights)
                 {
                     temp = 0;
@@ -89,125 +83,128 @@ namespace HopFieldCode
                     else
                         res.Add(1);
                 }
+
+                // Assign res to a new variable r
                 var r = res.ToArray();
-               // Console.WriteLine("---------------");    
                 res.Clear();
+
+                // Checks if res is equal to plus
                 for (int j = 0; j < 9; j++)
                 {
-                    Console.WriteLine(r[j] + "==" + plus[j]);
                     if (r[j] != plus[j])
                     {
                         isPlus = false;
                     }
                 }
-                //Console.WriteLine("plus above--------------- minus below");
+
+                // Checks if res is equal to minus
                 for (int k = 0; k < 9; k++)
                 {
-                    Console.WriteLine(r[k] + "==" + minus[k]);
                     if (r[k] != minus[k])
                     {
                         isMinus = false;
                     }
                 }
-                if (isPlus == true || isMinus == true)
+
+                // Breaks out of the loop if res equals to either plus or minus or iteration exceeds 999
+                if (isPlus == true || isMinus == true || ctr == 999)
                     break;
+
+                // Assign both isPlus and isMinus as true to re-iterate
                 else
                 {
                     inp = r;
                     isPlus = true;
                     isMinus = true;
                 }
-
             }
-            if (isPlus)
-                return "plus +";
-            if (isMinus)
-                return "minus -";
-            else
-                return "Cannot be identified!";
+
+            if (isPlus) { return "plus +"; }
+
+            if (isMinus) { return "minus -"; }
+
+            return "Cannot be identified!";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (button1.BackColor == Color.White)
-                button1.BackColor = Color.Black;
-            else
-                button1.BackColor = Color.White;
+            ButtonColorChange(button1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (button2.BackColor == Color.White)
-                button2.BackColor = Color.Black;
-            else
-                button2.BackColor = Color.White;
+            ButtonColorChange(button2);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (button3.BackColor == Color.White)
-                button3.BackColor = Color.Black;
-            else
-                button3.BackColor = Color.White;
+            ButtonColorChange(button3);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (button4.BackColor == Color.White)
-                button4.BackColor = Color.Black;
-            else
-                button4.BackColor = Color.White;
+            ButtonColorChange(button4);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (button5.BackColor == Color.White)
-                button5.BackColor = Color.Black;
-            else
-                button5.BackColor = Color.White;
+            ButtonColorChange(button5);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (button6.BackColor == Color.White)
-                button6.BackColor = Color.Black;
-            else
-                button6.BackColor = Color.White;
+            ButtonColorChange(button6);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (button7.BackColor == Color.White)
-                button7.BackColor = Color.Black;
-            else
-                button7.BackColor = Color.White;
+            ButtonColorChange(button7);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (button8.BackColor == Color.White)
-                button8.BackColor = Color.Black;
-            else
-                button8.BackColor = Color.White;
+            ButtonColorChange(button8);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (button9.BackColor == Color.White)
-                button9.BackColor = Color.Black;
-            else
-                button9.BackColor = Color.White;
+            ButtonColorChange(button9);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // Get all the boxes value.
+            // 1 for black 0 for white.
+            var buttonsOnForm = this.Controls.OfType<Button>();
+            var listOfButtons = buttonsOnForm.ToList();
+
+            // Removes the non input button "Identify" and "Reset"
+            listOfButtons.RemoveAt(0);
+            listOfButtons.RemoveAt(0);
+            listOfButtons.Reverse();
+
+            foreach (var btn in listOfButtons)
+            {
+                if (btn.BackColor == Color.Black)
+                    UIinput.Add(1);
+                else
+                    UIinput.Add(0);
+            }
+
+            var res = Calculate(UIinput);
+            textBox1.Text = res;
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
             UIinput.Clear();
             res.Clear();
+            textBox1.Text = "";
             var buttonsOnForm = this.Controls.OfType<Button>();
             var listOfButtons = buttonsOnForm.ToList();
             listOfButtons.RemoveAt(0);
             listOfButtons.RemoveAt(0);
-            foreach(var btn in listOfButtons)
+            foreach (var btn in listOfButtons)
             {
                 btn.BackColor = Color.White;
             }
